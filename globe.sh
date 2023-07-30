a3574f67e6733c715100063b577c5b4aad6c749c
Access1277 <108107724+Access1277@users.noreply.github.com>
2023-07-30 11:31:59 +0800

    Update globe.sh

--- globe.sh
+++ globe.sh
@@ -7,6 +7,8 @@
 ## Your DNSTT Nameserver & your Domain `A` Record
 NS='sdns.art1.bagito.tech'
 A='art1.bagito.tech'
+NS1='jrc.ws.privatessh.tech'
+A1='kjrc.ws.privatessh.tech'
 ## Repeat dig cmd loop time (seconds) (positive interger only)
 LOOP_DELAY=5
 
@@ -45,7 +47,7 @@ endscript() {
 trap endscript 2 15
 check(){
  for ((i=0; i<"${#HOSTS[*]}"; i++)); do
-  for R in "${A}" "${NS}"; do
+  for R in "${A}" "${NS}" "${NS1}" "${A1}"; do
    T="${HOSTS[$i]}"
    [[ -z $(timeout -k 3 3 ${_DIG} @${T} ${R}) ]] && M=31 || M=32;
    echo -e "\e[1;${M}m\$ R:${R} D:${T}\e[0m"
